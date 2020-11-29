735ed4dd7170dc0c819c64e50da8d4f113859506
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2020-11-29 15:52:29 +0000

    Done main.pl documentation

--- main.pl
+++ main.pl
@@ -10,7 +10,15 @@
 :- use_module(library(system)).
 :- use_module(library(between)).
 
-% play -> Starts the game, with a N x N board. Player1 (white color) starts playing.
+% ------------------------------------------------------------------------------------------------------------------------- %
+%                                                 Play - Start the app                                                      %
+%   Prototype:                                                                                                              %
+%       play/0                                                                                                              %
+%                                                                                                                           %
+%   Description:                                                                                                            %
+%       Starts running the app, displaying the menus. Player with White color starts playing.                               %
+% ------------------------------------------------------------------------------------------------------------------------- %
+
 play :-
     abolish(state/2),
     display_players_menu,
@@ -22,6 +30,19 @@ play :-
     start_game(G, N).
 
 play :- nl, write('Exiting Game...'), nl.
+
+% ------------------------------------------------------------------------------------------------------------------------- %
+%                                                     Start the Game                                                        %
+%   Prototype:                                                                                                              %
+%       start_game(+TypeGame, +Dimension)                                                                                   %
+%                                                                                                                           %
+%   Inputs:                                                                                                                 %
+%       TypeGame -> The type of the game to play (1 - Player VS Player, 2 - Player VS Computer, 3 - Computer VS Computer)   %
+%       Dimension -> The dimensions of the board chosen (7 - Board 7x7, 9 - Board 9x9, 11 - Board 11x11)                    %
+%                                                                                                                           %
+%   Outputs:                                                                                                                %
+%       Starts the game cycle, ends when a final board is reached.                                                          %
+% ------------------------------------------------------------------------------------------------------------------------- %
 
 start_game(1, N) :-
     nl, initial(N, InitialBoard), assert(state(white, InitialBoard)),
